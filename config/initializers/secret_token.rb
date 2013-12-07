93ecc51da35a08fa6f649f8610fe52f9e5b92486
Andrei Popa <andrei@audiostuff.ro>
2013-12-07 17:35:47 +0200

    Initial commit for scaffolding app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'd33262871b69a9a9ebf0de5dd3c3f0dda3cc226cf3e10097186aa66875f0cc75067550835698c0f46e6ee8e441edc5bc2da45d88959ad5e59f181cc7c8ec23c8'
